8855fd38fd12de1e15ca4f07c530d2872a1bf366
phatsk <phatsk@gmail.com>
2015-03-28 12:54:42 -0400

    Time test 2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 var UI = require('ui');
+var V = require('vector2');
 
 var face = new UI.Window();
-var time = new TimeText({text: '%a'});
+var timeText = new UI.TimeText({
+  position: new Vector2(0, 25),
+  size: new Vector2(144, 30),
+  text: "%H:%M",
+  font: 'bitham-42-bold',
+  color: 'black',
+  textAlign: 'center'
+});
 
-face.add(time);
+face.add(timeText);
 
 face.show();
